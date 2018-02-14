b8200a869e4498182e9545ac2512360872f48ee2
Carlos Madrigal N <cmadrigaln@ucenfotec.ac.cr>
2018-02-14 14:56:07 -0600

    Add project files.

---
+++ LAB04-EXCEPTION_MANAGEMENT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB04_EXCEPTION_MANAGEMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
